562187e35e8075d1d66afed2a1902841f1fba4d7
marshalltaylor <silvercraft@gmail.com>
2016-09-30 01:05:16 -0600

    moved constants to devRegisters.c from main.c

--- Firmware/PSoC Projects/FirmwareBuild/FW002.cydsn/devRegisters.c
+++ Firmware/PSoC Projects/FirmwareBuild/FW002.cydsn/devRegisters.c
@@ -1,5 +1,11 @@
+#include <project.h>
 #include "devRegisters.h"
 #include "stdint.h"
+
+// Configuration constants from main
+extern const uint8_t ID_WORD; //Device ID to be programmed into memory for reads
+extern const uint8_t START_SLAVE_ADDR;//Start address of slaves
+extern const uint8_t MAX_SLAVE_ADDR;//Max address of slaves
 
 #define REGISTER_TABLE_LENGTH 128
 
@@ -14,6 +20,50 @@ void initDevRegisters( void )
         registerTable[i] = 0x00;
         registerChangedTable[i] = 0x00;
     }
+    writeDevRegister(SCMD_STATUS, 0x1A);
+    writeDevRegister(SCMD_ID, ID_WORD);
+    writeDevRegister(SCMD_CONFIG_BITS, CONFIG_BITS_REG_Read() ^ 0x0F);    // Read HW config bits
+    writeDevRegister(SCMD_FSAFE_TIME, 0 );
+    writeDevRegister(SCMD_MA_DRIVE, 0x80);
+    writeDevRegister(SCMD_MB_DRIVE, 0x80);
+    writeDevRegister(SCMD_REM_OFFSET, 0x01);
+    writeDevRegister(SCMD_REM_ADDR, 0x4A);
+    writeDevRegister(SCMD_SLV_POLL_CNT, 0);
+    writeDevRegister(SCMD_SLAVE_ADDR, 0x10); // No one should ever ask for data on 0x10
+    writeDevRegister(SCMD_BAUD_RATE, 0x07);
+    writeDevRegister(SCMD_S1A_DRIVE, 0x80);
+    writeDevRegister(SCMD_S1B_DRIVE, 0x80);
+    writeDevRegister(SCMD_S2A_DRIVE, 0x80);
+    writeDevRegister(SCMD_S2B_DRIVE, 0x80);
+    writeDevRegister(SCMD_S3A_DRIVE, 0x80);
+    writeDevRegister(SCMD_S3B_DRIVE, 0x80);
+    writeDevRegister(SCMD_S4A_DRIVE, 0x80);
+    writeDevRegister(SCMD_S4B_DRIVE, 0x80);
+    writeDevRegister(SCMD_S5A_DRIVE, 0x80);
+    writeDevRegister(SCMD_S5B_DRIVE, 0x80);
+    writeDevRegister(SCMD_S6A_DRIVE, 0x80);
+    writeDevRegister(SCMD_S6B_DRIVE, 0x80);
+    writeDevRegister(SCMD_S7A_DRIVE, 0x80);
+    writeDevRegister(SCMD_S7B_DRIVE, 0x80);
+    writeDevRegister(SCMD_S8A_DRIVE, 0x80);
+    writeDevRegister(SCMD_S8B_DRIVE, 0x80);
+    writeDevRegister(SCMD_S9A_DRIVE, 0x80);
+    writeDevRegister(SCMD_S9B_DRIVE, 0x80);
+    writeDevRegister(SCMD_S10A_DRIVE, 0x80);
+    writeDevRegister(SCMD_S10B_DRIVE, 0x80);
+    writeDevRegister(SCMD_S11A_DRIVE, 0x80);
+    writeDevRegister(SCMD_S11B_DRIVE, 0x80);
+    writeDevRegister(SCMD_S12A_DRIVE, 0x80);
+    writeDevRegister(SCMD_S12B_DRIVE, 0x80);
+    writeDevRegister(SCMD_S13A_DRIVE, 0x80);
+    writeDevRegister(SCMD_S13B_DRIVE, 0x80);
+    writeDevRegister(SCMD_S14A_DRIVE, 0x80);
+    writeDevRegister(SCMD_S14B_DRIVE, 0x80);
+    writeDevRegister(SCMD_S15A_DRIVE, 0x80);
+    writeDevRegister(SCMD_S15B_DRIVE, 0x80);
+    writeDevRegister(SCMD_S16A_DRIVE, 0x80);
+    writeDevRegister(SCMD_S16B_DRIVE, 0x80);
+
 }
 
 uint8_t readDevRegister( uint8_t regNumberIn )
--- Firmware/PSoC Projects/FirmwareBuild/FW002.cydsn/main.c
+++ Firmware/PSoC Projects/FirmwareBuild/FW002.cydsn/main.c
@@ -22,87 +22,14 @@ volatile uint8_t CONFIG_BITS = 0x3;
     //2 -- I2C slave on slave port
     //3 -- I2C @ 0x58
 
-#define ID_WORD 0xA9 //Device ID to be programmed into memory for reads
-#define START_SLAVE_ADDR 0x50//Start address of slaves
-#define MAX_SLAVE_ADDR 0x5F//Max address of slaves
+// Configuration constants
+const uint8_t ID_WORD            = 0xA9;//Device ID to be programmed into memory for reads
+const uint8_t START_SLAVE_ADDR   = 0x50;//Start address of slaves
+const uint8_t MAX_SLAVE_ADDR     = 0x5F;//Max address of slaves
 
-#define SCMD_STATUS                0x00
-#define SCMD_ID                    0x01
-#define SCMD_SLAVE_ADDR            0x02
-#define SCMD_CONFIG_BITS           0x03
-#define SCMD_I2C_FAULTS            0x04
-#define SCMD_I2C_RD_ERR            0x05
-#define SCMD_I2C_WR_ERR            0x06
-#define SCMD_SPI_FAULTS            0x07
-#define SCMD_UART_FAULTS           0x08
-#define SCMD_UPORT_TIME	           0x09
-#define SCMD_SLV_POLL_CNT          0x0A
-#define SCMD_SLV_TOP_ADDR          0x0B
-#define SCMD_FSAFE_TIME            0x0C
-#define SCMD_FSAFE_FAULTS          0x0D
-                                  
-#define SCMD_SLAVE_ID              0x10
-#define SCMD_REM_ADDR              0x11
-#define SCMD_REM_OFFSET            0x12
-#define SCMD_REM_DATA_WR           0x13
-#define SCMD_REM_DATA_RD           0x14
-#define SCMD_REM_WRITE             0x15
-#define SCMD_REM_READ              0x16
-
-#define SCMD_MOTOR_A_INVERT        0x17
-#define SCMD_MOTOR_B_INVERT        0x18
-#define SCMD_BRIDGE                0x19
-
-#define SCMD_BAUD_RATE             0x1E
-                                
-#define SCMD_MA_DRIVE              0x20
-#define SCMD_MB_DRIVE              0x21
-#define SCMD_S1A_DRIVE             0x22
-#define SCMD_S1B_DRIVE             0x23
-#define SCMD_S2A_DRIVE             0x24
-#define SCMD_S2B_DRIVE             0x25
-#define SCMD_S3A_DRIVE             0x26
-#define SCMD_S3B_DRIVE             0x27
-#define SCMD_S4A_DRIVE             0x28
-#define SCMD_S4B_DRIVE             0x29
-#define SCMD_S5A_DRIVE             0x2A
-#define SCMD_S5B_DRIVE             0x2B
-#define SCMD_S6A_DRIVE             0x2C
-#define SCMD_S6B_DRIVE             0x2D
-#define SCMD_S7A_DRIVE             0x2E
-#define SCMD_S7B_DRIVE             0x2F
-#define SCMD_S8A_DRIVE             0x30
-#define SCMD_S8B_DRIVE             0x31
-#define SCMD_S9A_DRIVE             0x32
-#define SCMD_S9B_DRIVE             0x33
-#define SCMD_S10A_DRIVE            0x34
-#define SCMD_S10B_DRIVE            0x35
-#define SCMD_S11A_DRIVE            0x36
-#define SCMD_S11B_DRIVE            0x37
-#define SCMD_S12A_DRIVE            0x38
-#define SCMD_S12B_DRIVE            0x39
-#define SCMD_S13A_DRIVE            0x3A
-#define SCMD_S13B_DRIVE            0x3B
-#define SCMD_S14A_DRIVE            0x3C
-#define SCMD_S14B_DRIVE            0x3D
-#define SCMD_S15A_DRIVE            0x3E
-#define SCMD_S15B_DRIVE            0x3F
-#define SCMD_S16A_DRIVE            0x40
-#define SCMD_S16B_DRIVE            0x41
-
-#define SCMD_INV_2_9               0x60
-#define SCMD_INV_10_17             0x61
-#define SCMD_INV_18_25             0x62
-#define SCMD_INV_26_33             0x63
-#define SCMD_BRIDGE_SLV_L          0x64
-#define SCMD_BRIDGE_SLV_H          0x65
-
-#define SCMD_DRIVER_ENABLE         0x70
-
-
+uint8_t slaveAddrEnumerator;
 
 #define USER_PORT_BUFFER_SIZE (10u)
-
 
 /* Common buffers or I2C and UART */
 uint8 bufferRx[USER_PORT_BUFFER_SIZE + 1u];/* RX software buffer requires one extra entry for correct operation in UART mode */
@@ -382,7 +309,6 @@ uint8_t slaveState = SCMDSlaveIdle;
 
 uint8_t masterState = SCMDMasterIdle;
 
-uint8_t slaveAddrEnumerator = START_SLAVE_ADDR;
 /*******************************************************************************
 * Define Interrupt service routine and allocate an vector to the Interrupt
 ********************************************************************************/
@@ -477,53 +403,8 @@ uint8 WriteSlaveData( uint8_t address, uint8_t offset, uint8_t data )
 
 int main()
 {
-    initDevRegisters();  //Prep device registers
-    writeDevRegister(SCMD_STATUS, 0x1A);
-    writeDevRegister(SCMD_ID, ID_WORD);
-    writeDevRegister(SCMD_CONFIG_BITS, CONFIG_BITS_REG_Read() ^ 0x0F);    // Read HW config bits
-    writeDevRegister(SCMD_FSAFE_TIME, 0 );
-    writeDevRegister(SCMD_MA_DRIVE, 0x80);
-    writeDevRegister(SCMD_MB_DRIVE, 0x80);
-    writeDevRegister(SCMD_REM_OFFSET, 0x01);
-    writeDevRegister(SCMD_REM_ADDR, 0x4A);
-    writeDevRegister(SCMD_SLV_POLL_CNT, 0);
-    writeDevRegister(SCMD_SLAVE_ADDR, 0x10); // No one should ever ask for data on 0x10
-    writeDevRegister(SCMD_BAUD_RATE, 0x07);
-    writeDevRegister(SCMD_S1A_DRIVE, 0x80);
-    writeDevRegister(SCMD_S1B_DRIVE, 0x80);
-    writeDevRegister(SCMD_S2A_DRIVE, 0x80);
-    writeDevRegister(SCMD_S2B_DRIVE, 0x80);
-    writeDevRegister(SCMD_S3A_DRIVE, 0x80);
-    writeDevRegister(SCMD_S3B_DRIVE, 0x80);
-    writeDevRegister(SCMD_S4A_DRIVE, 0x80);
-    writeDevRegister(SCMD_S4B_DRIVE, 0x80);
-    writeDevRegister(SCMD_S5A_DRIVE, 0x80);
-    writeDevRegister(SCMD_S5B_DRIVE, 0x80);
-    writeDevRegister(SCMD_S6A_DRIVE, 0x80);
-    writeDevRegister(SCMD_S6B_DRIVE, 0x80);
-    writeDevRegister(SCMD_S7A_DRIVE, 0x80);
-    writeDevRegister(SCMD_S7B_DRIVE, 0x80);
-    writeDevRegister(SCMD_S8A_DRIVE, 0x80);
-    writeDevRegister(SCMD_S8B_DRIVE, 0x80);
-    writeDevRegister(SCMD_S9A_DRIVE, 0x80);
-    writeDevRegister(SCMD_S9B_DRIVE, 0x80);
-    writeDevRegister(SCMD_S10A_DRIVE, 0x80);
-    writeDevRegister(SCMD_S10B_DRIVE, 0x80);
-    writeDevRegister(SCMD_S11A_DRIVE, 0x80);
-    writeDevRegister(SCMD_S11B_DRIVE, 0x80);
-    writeDevRegister(SCMD_S12A_DRIVE, 0x80);
-    writeDevRegister(SCMD_S12B_DRIVE, 0x80);
-    writeDevRegister(SCMD_S13A_DRIVE, 0x80);
-    writeDevRegister(SCMD_S13B_DRIVE, 0x80);
-    writeDevRegister(SCMD_S14A_DRIVE, 0x80);
-    writeDevRegister(SCMD_S14B_DRIVE, 0x80);
-    writeDevRegister(SCMD_S15A_DRIVE, 0x80);
-    writeDevRegister(SCMD_S15B_DRIVE, 0x80);
-    writeDevRegister(SCMD_S16A_DRIVE, 0x80);
-    writeDevRegister(SCMD_S16B_DRIVE, 0x80);
-    
-    
-    
+    initDevRegisters();  //Prep device registers, set initial values (triggers actions)
+    slaveAddrEnumerator = START_SLAVE_ADDR;  
 #ifndef USE_SW_CONFIG_BITS
     CONFIG_BITS = readDevRegister(SCMD_CONFIG_BITS); //Get the bits value
 #endif
